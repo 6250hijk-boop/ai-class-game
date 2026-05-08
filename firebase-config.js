4d9860ea32c1b775c9ac5fa091c62f2463695ee8
6250hijk-boop <6250hijk@gmail.com>
2026-05-08 10:42:47 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -19,7 +19,7 @@ const GDRIVE_CONFIG = {
   API_KEY: "AIzaSyAPfaW3Vb9beLdcgGlus1ZMADRhjIXpmwE",
   SCOPES: "https://www.googleapis.com/auth/drive.file",
   // Apps Script 웹앱 배포 후 URL 입력
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzt3-5Uy1L1t1ciRGs6RNE-Kv0saVwxyugFJIK7GT2pSXWAa0NQ_KXCa8xC2CihxzQd/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbweknZnY6hin7FJXWuOidNv-hTqCkb7eF97rRXofGAfUucU6r6FpQe-dcfvlIfPt4gDLg/exec",
 };
 
 firebase.initializeApp(firebaseConfig);
